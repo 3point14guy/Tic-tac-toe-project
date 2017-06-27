7d7f6f26dbd75dc7ae9ee3cadb88a5412bde31dc
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-06-27 14:52:28 -0400

    saving initial html css and js
    -got rough outline of html in started JS

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,8 +7,35 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
+$('.square-one').on('click', function () {
+  $('.square-one').text('x')
+})
+$('.square-two').on('click', function () {
+  $('.square-two').text('x')
+})
+$('.square-three').on('click', function () {
+  $('.square-three').text('x')
+})
+$('.square-four').on('click', function () {
+  $('.square-four').text('x')
+})
+$('.square-five').on('click', function () {
+  $('.square-five').text('x')
+})
+$('.square-six').on('click', function () {
+  $('.square-six').text('x')
+})
+$('.square-seven').on('click', function () {
+  $('.square-seven').text('x')
+})
+$('.square-eight').on('click', function () {
+  $('.square-eight').text('x')
+})
+$('.square-nine').on('click', function () {
+  $('.square-nine').text('x')
+})
+// $('.squareOne').on('click', insertMarker() {
+//     squareone = 'x'
+// })
 
-// use require without a reference to ensure a file is bundled
 require('./example')
