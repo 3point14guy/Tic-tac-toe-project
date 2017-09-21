9bf878625dc7bafae96c82862687d0bae53429c9
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-09-21 17:24:45 -0400

    corrected link for hosted API
    removed back slash from end of link

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,5 @@
 'use strict'
 
 module.exports = {
-  host: 'https://powerful-scrubland-54674.herokuapp.com/'
+  host: 'https://powerful-scrubland-54674.herokuapp.com'
 }
-// temporarily replacing 'https://ga-library-api.herokuapp.com' with
-// http://tic-tac-toe.wdibos.com
-// now trying https://powerful-scrubland-54674.herokuapp.com/  ****this last one works
-// don't know why the library one was working at first
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     development: 'http://tic-tac-toe.wdibos.com',
-    production: 'https://powerful-scrubland-54674.herokuapp.com/'
+    production: 'https://powerful-scrubland-54674.herokuapp.com'
   }
 }
 
